00b5952c3494f74ec834cd88dd778ac86809ad40
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-03-12 08:55:20 +0530

    Time: 3 ms (6.18%) | Memory: 8.5 MB (20.80%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,17 +3,20 @@ public:
     bool rotateString(string s, string goal) {
         int n = s.length();
 
+        // if lengths of s and goal not equal , rot not possible
         if(s.length()!= goal.length()){
             return false;
         }
 
         int i =0;
 
+        //rotate s for all possible comb
         while(i<n){
             string temp = s;
             for(int j=0;j<n;j++){
                 temp[(j+i)%n] = s[j];
             }
+            //if any rot is equal to goal
             if(temp == goal){
                 return true;
             }
