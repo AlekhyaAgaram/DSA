16b25ff2f036e326fe0231ea46f34962014bd90f
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-03-19 11:59:13 +0530

    Time: 4 ms (75.69%) | Memory: 12.8 MB (66.72%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,18 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        res = []
         l = 0
         r = len(numbers) - 1
-        flag = False
 
-        while(flag == False):
+        while(l<r):
             if((numbers[l]+numbers[r]) == target):
-                res.append(l+1)
-                res.append(r+1)
-                flag = True
+                return [l+1,r+1]
             elif(numbers[l]+numbers[r] < target):
                 l = l + 1
             elif(numbers[l]+numbers[r] > target):            
                 r = r - 1
-        return res
