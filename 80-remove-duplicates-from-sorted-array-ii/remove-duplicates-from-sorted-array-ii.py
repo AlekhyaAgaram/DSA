96898aa1fa03a2ab54e6ff194b40ef8fbd43cd56
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-03-19 12:42:10 +0530

    Time: 75 ms (32.23%) | Memory: 15.4 MB (52.26%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #if number <2, alreavdy valid
         if len(nums) <= 2:
             return len(nums)
 
         i = 2 
         for j in range(2, len(nums)):
+            # if current number is not equal to 2 numbers before  = keep it
             if nums[j] != nums[i-2]:
                 nums[i] = nums[j]
                 i += 1
