c8dd5bcf571621f8e48b4cbcff1377f85d4ec8b5
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-03-24 06:17:08 +0530

    Time: 744 ms (7.58%) | Memory: 0B (100.00%) - LeetSync

--- SQL_50/197-rising-temperature/rising-temperature.sql
+++ SQL_50/197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
+
+/*
+have to do self joing on weather table
+can not subtract 1 from date so hv to use datediff function
+*/
+
 select w1.id from Weather w1 JOIN weather w2 
 on datediff(w1.recordDate,w2.recordDate) = 1
 where w1.temperature > w2.temperature;
