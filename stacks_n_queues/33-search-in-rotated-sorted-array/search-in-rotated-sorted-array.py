1d606ff31bcd3bb51a285b5b6a15142c43bdf997
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-06-11 11:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (95.94%) - LeetSync

--- stacks_n_queues/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ stacks_n_queues/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -10,14 +10,14 @@ class Solution:
 
             if nums[l] <= nums[mid]:
                 #
-                if target > nums[mid] or target < nums[l]:
-                    l = mid+1
-                else:
-                    r = mid - 1
-            else:
-                if target < nums[mid] or target > nums[r]:
+                if nums[l] <= target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                     r = mid - 1
         return -1
             
