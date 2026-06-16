05697a02a014946f458328a29e9c730ab8c0ecf7
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-06-16 20:20:39 +0530

    Time: 3 ms (53.09%) | Memory: 20.4 MB (70.65%) - LeetSync

---
+++ stacks_n_queues/110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+            
+        def dfs(node):
+            if node is None:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            return 1 + max(left,right)
+
+        if root is None:
+            return True
+        queue = [root]
+
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.pop(0)
+                
+                left = dfs(node.left)
+                right = dfs(node.right)
+                if abs(left - right) > 1:
+                    return False
+
+                if node.left:
+                    queue.append(node.left)
+                
+                if node.right:
+                    queue.append(node.right)
+                
+        return True
