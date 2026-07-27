1470679b23479108a863623cd8c3224ed27b7924
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-07-27 11:09:14 +0530

    Time: 20 ms (94.63%) | Memory: 31.6 MB (19.55%) - LeetSync

--- revision/53-maximum-subarray/maximum-subarray.py
+++ revision/53-maximum-subarray/maximum-subarray.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
+        """        
         n = len(nums)
         res = [0]*n
         res[0] = nums[0]
@@ -11,5 +11,19 @@ class Solution:
         for i in range(1,n):
             res[i] = max(nums[i],res[i-1]+nums[i])
         return max(res)
+        """
+        curr = 0
+        maxi = nums[0]
+
+        for i in range(len(nums)):
+            curr += nums[i]
+            if maxi < curr:
+                maxi = curr
+
+            if curr < 0:
+                curr = 0
+
+            
+        return maxi
 
         
