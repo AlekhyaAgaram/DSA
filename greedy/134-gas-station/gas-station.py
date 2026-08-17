aaa6f3d07f57ecd7771c16360639e8dc26586102
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-08-17 13:04:45 +0530

    Time: 30 ms (35.75%) | Memory: 26 MB (67.46%) - LeetSync

--- greedy/134-gas-station/gas-station.py
+++ greedy/134-gas-station/gas-station.py
@@ -3,6 +3,7 @@ class Solution:
 
         if sum(gas) < sum(cost):
             return -1
+
         
         total = 0
         res = 0
@@ -12,4 +13,4 @@ class Solution:
                 total = 0
                 res = i+1
         return res
-
+        
