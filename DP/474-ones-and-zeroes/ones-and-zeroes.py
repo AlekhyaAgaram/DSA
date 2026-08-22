558a4e7d6f337efab87d59f7a7ae3541d59816e1
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-08-22 23:24:24 +0530

    Time: 1676 ms (23.52%) | Memory: 336.2 MB (24.49%) - LeetSync

---
+++ DP/474-ones-and-zeroes/ones-and-zeroes.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+
+        memo = {}
+        def dfs(m,n,i):
+
+            if i < 0:
+                return 0
+
+            if (m,n,i) in memo:
+                return memo[(m,n,i)]
+
+            no_1 = 0
+            no_0 = 0
+            for j in strs[i]:
+                if j == '1':
+                    no_1 += 1
+                else:
+                    no_0 += 1
+
+            take = 0
+            skip = 0
+
+            if no_1 <= n and no_0 <= m:
+                take = 1 + dfs(m-no_0, n-no_1, i-1)
+            skip = dfs(m,n,i-1) 
+
+            memo[(m,n,i)] =  max(take,skip)
+            return memo[(m,n,i)]
+        return dfs(m,n,len(strs)-1)
