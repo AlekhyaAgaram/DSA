521026efea5cdd02bff7ed9919a92bdf1ed87bb9
AlekhyaAgaram <alekhya.agaram@gmail.com>
2026-08-22 23:31:33 +0530

    Time: 946 ms (69.40%) | Memory: 19.4 MB (73.64%) - LeetSync

--- DP/474-ones-and-zeroes/ones-and-zeroes.py
+++ DP/474-ones-and-zeroes/ones-and-zeroes.py
@@ -1,6 +1,7 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
 
+        """
         memo = {}
         def dfs(m,n,i):
 
@@ -28,3 +29,18 @@ class Solution:
             memo[(m,n,i)] =  max(take,skip)
             return memo[(m,n,i)]
         return dfs(m,n,len(strs)-1)
+
+        """
+
+        #BOTTOM UP APPROACH
+        dp = [[0]*(n+1) for _ in range(m+1)] 
+
+        for s in strs:
+            zero = s.count('0')
+            one = s.count('1')
+
+            for i in range(m, zero-1, -1):
+                for j in range(n, one-1 , -1):
+                    dp[i][j] = max(dp[i][j], 1+ dp[i-zero][j-one])
+        return dp[m][n]
+                        
